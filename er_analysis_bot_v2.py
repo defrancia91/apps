bc861a3d9dcabcf4c9714b01cd8a060b19e0a5f8
defrancia91 <em.molina@outlook.com>
2024-07-22 23:04:43 +0200

    Update er_analysis_bot_v2.py

--- er_analysis_bot_v2.py
+++ er_analysis_bot_v2.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pypdf
 import json
 import requests
+from PIL import Image
 
 # Function to read PDF and extract text
 def read_pdf(file):
@@ -31,8 +32,11 @@ st.markdown(
 # Set Sidebar
 
 with st.sidebar:
-  hf_api_key = st.text_input("Enter your Hugging Face API key", type="password")
-  ngrok_auth_token = st.text_input("Enter your NGROK token", type="password")
+	hf_api_key = st.text_input("Enter your Hugging Face API key", type="password")
+	ngrok_auth_token = st.text_input("Enter your NGROK token", type="password")
+	sidebarlogo = Image.open('logo2.png').resize((300, 390))
+        st.image(sidebarlogo, use_column_width='auto')
+	
 
 # Upload the file
 uploaded_file = st.file_uploader("Upload a file", type=("txt", "md", "pdf"))
